a6062cb15d9b59230242a221745de3c0059105d8
MoamrMohammed <89658193+MoamrMohammed@users.noreply.github.com>
2021-08-31 16:13:52 +0200

    Add files via upload

---
+++ Sub.kt
@@ -0,0 +1,56 @@
+fun main(args:arry <string>){
+
+/*
+    [+] Addition
+*/
+	var a = 8
+	var b = 4
+	var g = a + b
+	print(g)
+
+
+
+
+
+
+/*
+    [-] Subtraction
+
+*/
+	var a = 8
+	var b = 4
+	var y = a - b
+	print(y)
+
+
+
+/*
+    [*] Multiplication
+*/
+    var a = 8
+	var b = 4
+	var x = a + b
+	print(x)
+
+
+
+/*
+    [/] Dvision
+
+*/
+    var a = 8
+	var b = 4
+	var r = a / b
+	print(r)
+
+
+/*
+    [%] Moduluus
+
+*/
+   var a = 9
+	var b = 4
+	var c = a % b
+	print(c)
+
+}
